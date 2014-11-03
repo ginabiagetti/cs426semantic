5fd9e90d2eb82bda427e2a5ab6dbe5c1caf93f91
victoriarathbone <c15victoria.rathbone@usafa.edu>
2014-11-03 08:34:35 -0700

    change

--- CS426Compiler/CS426Compiler/SemanticAnalyzer.cs
+++ CS426Compiler/CS426Compiler/SemanticAnalyzer.cs
@@ -14,7 +14,16 @@ namespace CS426Compiler
        
         public override void InAProgram(comp5210.node.AProgram node)
         {
+            BasicType inttype = new BasicType();
+            inttype.name = "int";
+            BasicType flttype = new BasicType();
+            flttype.name = "float";
+            stringhash.Add(inttype.name, inttype);
+            stringhash.Add(flttype.name, flttype);
+
             base.InAProgram(node);
         }
+
+        
     }
 }
